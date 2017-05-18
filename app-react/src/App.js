33b93b56b0d292dae9905b1518268174ef41c2b7
Julius Williams <jmichaelcodes@gmail.com>
2017-05-18 14:18:02 -0400

    fixed face cards, cleaned up code

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -14,35 +14,66 @@ class App extends Component {
       diamonds: [],
       hearts: [],
       spades: [],
-      deck: []
+      deck: [],
+      hand: [],
+      card1: '',
+      card2: '',
+      card3: '',
+      card4: '',
+      card5: '',
     }
 
-    this.addValuesToCards = this.addValuesToCards.bind(this)
     this.addCardsToDeck = this.addCardsToDeck.bind(this)
+    this.setHand = this.setHand.bind(this)
+    this.shuffle = this.shuffle.bind(this)
 
   }
 
   componentWillMount() {
-    this.addValuesToCards()
+    this.shuffle()
+  }
+
+  shuffle() {
     this.addCardsToDeck("_of_clubs")
     this.addCardsToDeck("_of_diamonds")
     this.addCardsToDeck("_of_hearts")
     this.addCardsToDeck("_of_spades")
+    this.setHand()
   }
-
-addValuesToCards() {
-  for (var i = 0; i < 13; i++) {
-    this.setState({ cardValues: this.state.cardValues.push(i) })
-  }
-  console.log(this.state.cardValues)
-}
 
 addCardsToDeck(ofSuit){
-    for (var i = 0; i < this.state.cardValues.length; i++) {
+    for (var i = 2; i < 11; i++) {
     this.setState({ deck: this.state.deck.push(i + ofSuit)})
   }
+
+  this.setState({deck: this.state.deck.push("ace" + ofSuit)})
+  this.setState({deck: this.state.deck.push("jack" + ofSuit)})
+  this.setState({deck: this.state.deck.push("queen" + ofSuit)})
+  this.setState({deck: this.state.deck.push("king" + ofSuit)})
+
   console.log(this.state.deck)
 }
+
+setHand() {
+
+  for (var i = 0; i < 5; i++) {
+    var j = Math.floor(Math.random() * this.state.deck.length - 1) + 1
+    console.log(j)
+    this.setState({ hand: this.state.hand.push(this.state.deck[j])})
+    this.setState({ deck: this.state.deck.splice(j, 1)})
+  }
+
+  this.setState({ card1: this.state.hand[0]})
+  this.setState({ card2: this.state.hand[1]})
+  this.setState({ card3: this.state.hand[2]})
+  this.setState({ card4: this.state.hand[3]})
+  this.setState({ card5: this.state.hand[4]})
+
+  console.log("hand state " + this.state.hand)
+  console.log("deck state " + this.state.deck)
+}
+
+
 
   render() {
     return (
@@ -51,7 +82,9 @@ addCardsToDeck(ofSuit){
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <Cards card1="2_of_clubs"/>
+        <Cards card1={this.state.card1} card2={this.state.card2} card3={this.state.card3} card4={this.state.card4} card5={this.state.card5} />
+        <br/><br/><br/> 
+        <div className="btn btn-lg btn-success" onClick={this.shuffle}>Shuffle</div>
       </div>
     );
   }
