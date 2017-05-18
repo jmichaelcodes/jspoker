62508d2d161500d49b595d4cd73cab9ac0d3e572
Julius Williams <jmichaelcodes@gmail.com>
2017-05-18 12:29:32 -0400

    cards adding to deck

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -5,6 +5,45 @@ import './App.css';
 import Cards from "./components/Cards"
 
 class App extends Component {
+    constructor(props) {
+    super(props)
+
+    this.state = {
+      cardValues: [],
+      clubs: [],
+      diamonds: [],
+      hearts: [],
+      spades: [],
+      deck: []
+    }
+
+    this.addValuesToCards = this.addValuesToCards.bind(this)
+    this.addCardsToDeck = this.addCardsToDeck.bind(this)
+
+  }
+
+  componentWillMount() {
+    this.addValuesToCards()
+    this.addCardsToDeck("_of_clubs")
+    this.addCardsToDeck("_of_diamonds")
+    this.addCardsToDeck("_of_hearts")
+    this.addCardsToDeck("_of_spades")
+  }
+
+addValuesToCards() {
+  for (var i = 0; i < 13; i++) {
+    this.setState({ cardValues: this.state.cardValues.push(i) })
+  }
+  console.log(this.state.cardValues)
+}
+
+addCardsToDeck(ofSuit){
+    for (var i = 0; i < this.state.cardValues.length; i++) {
+    this.setState({ deck: this.state.deck.push(i + ofSuit)})
+  }
+  console.log(this.state.deck)
+}
+
   render() {
     return (
       <div className="App">
@@ -12,7 +51,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <Cards/>
+        <Cards card1="2_of_clubs"/>
       </div>
     );
   }
--- app-react/src/components/Cards.js
+++ app-react/src/components/Cards.js
@@ -15,11 +15,12 @@ class Cards extends React.Component {
 
         return <div className="container cards" >
 
-            <img className="col-sm-offset-1 col-sm-2" src={this.props.card1}/>
-            <img className="col-sm-2" src={this.props.card2}/>
-            <img className="col-sm-2" src={this.props.card3}/>
-            <img className="col-sm-2" src={this.props.card4}/>
-            <img className="col-sm-2" src={this.props.card5}/>
+            {/*<img className="col-sm-offset-1 col-sm-2" src={this.props.card1}/>*/}
+            <img className="col-sm-offset-1 col-sm-2" src={"../img/cards/" + this.props.card1 + ".png"}/>
+            <img className="col-sm-2" src={"../img/cards/" + this.props.card2 + ".png"}/>
+            <img className="col-sm-2" src={"../img/cards/" + this.props.card3 + ".png"}/>
+            <img className="col-sm-2" src={"../img/cards/" + this.props.card4 + ".png"}/>
+            <img className="col-sm-2" src={"../img/cards/" + this.props.card5 + ".png"}/>
 
         </div>
     }
